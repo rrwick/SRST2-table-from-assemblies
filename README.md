7f50911f56d50860feeb31b0579cc50283bd5a20
Ryan Wick <rrwick@gmail.com>
2016-02-16 18:12:14 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# SRST2 results from assemblies
+
+This is a tool for conducting a gene screen on assemblies.  It produces a table which mimics the format of [SRST2](https://github.com/katholt/srst2)'s compiled results.
+
+It can be useful when there are some samples for which you have reads and other samples for which you only have assemblies.  You can then use SRST2 to conduct a gene screen on the reads and this script to conduct a gene screen on the assemblies.  The results can then be combined using SRST2.  Note that if you have both reads and assemblies, it is preferrable to use SRST2 on the reads instead of this script (better sensitivity).
+
+It uses [BLAST+](http://www.ncbi.nlm.nih.gov/books/NBK279690/) to conduct the gene searches and requires BLAST+ to be installed.
+
+## Usage:
+```
+srst2_results_from_assemblies.py [-h] --assemblies ASSEMBLIES [ASSEMBLIES ...]
+                                 --gene_db GENE_DB
+                                 --output OUTPUT
+                                 [--min_coverage MIN_COVERAGE]
+                                 [--max_divergence MAX_DIVERGENCE]
+                                 [--report_new_consensus]
+                                 [--report_all_consensus]
+                                 [--algorithm ALGORITHM]
+```
+
+`--assemblies`: FASTA files of all assemblies to screen.  The sample name will be taken from the assembly filename (without the `.fasta` or `.fa` extension)
+
+`--gene_db`: a gene database to search for in [SRST2 format](https://github.com/katholt/srst2#generating-srst2-compatible-clustered-database-from-raw-sequences)
+
+`--output`: the filename for the resulting table
+
+`--min_coverage`: the minimum allowed BLAST hit percent coverage for an allele (default = 90)
+
+`--max_divergence`: the maximum allowed percent divergence between a BLAST hit and an allele sequence (default = 10)
+
+`--report_new_consensus`: save all novel alleles (those which do not exactly match any in the gene database) to a FASTA file named `new_consensus_alleles.fasta`
+
+`--report_all_consensus`: save all found alleles (both those in the gene database and novel alleles) to a FASTA file named `all_consensus_alleles.fasta`
+
+`--algorithm`: which BLAST+ algorithm to use (`blastn`, `blastn-short`, `megablast` or `dc-megablast`, default = `blastn`)
+
+### Example command
+
+`srst2_results_from_assemblies.py --assemblies *.fasta --gene_db resistance_genes.mfasta --output table.txt --report_new_consensus`
+
+This command will:
+* Screen every one of the assemblies (all `*.fasta` files) for each of the genes in `resistance_genes.mfasta` using `blastn`
+* Save a table of results to `table.txt`
+* Save a FASTA file of any new alleles to `new_consensus_alleles.fasta`
+
+### Example gene database
+
+```
+>0__abcA__abcA_1__0
+ATGGACTTTTCCCGCTTTTATATCGACAGGCCGATCTTC
+>0__abcA__abcA_2__1
+ATGGACTTTTCCCGCTTTTTTATCGACAGGCCGATTTTC
+>1__abcB__abcB_1__2
+ATGAGAAATAAAGGAATCGATCAATTTTGTGTGATTGCA
+>1__abcB__abcB_2__3
+ATGAAAAATAAAGGAATCGATCAATTTCGTGTGATTGCA
+```
+
+### Example output table
+
+Sample | abcA | abcB
+--- | --- | --- |
+sample1 | abcA_1 | abcB_1 |
+sample2 | abcA_1 | abcB_2* |
+sample3 | abcA_2 | - |
+
+As is the case for SRST2:
+* Imperfect matches (containing at least one mismatch or indel) are indicated with a `*` after the allele name.
+* Absent genes are indicated with `-`.
+
+## License
+
+GNU General Public License, version 3
---
+++ srst2_table_from_assemblies.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+'''
+SRST2 results from assemblies
+
+This is a tool to screen for genes in a collection of assemblies and output
+the results in a table which mimics those produced by SRST2.
+
+Author: Ryan Wick
+email: rrwick@gmail.com
+'''
+
+from __future__ import print_function
+from __future__ import division
+import sys
+import argparse
+import subprocess
+import os
+from distutils import spawn
+from sets import Set
+
+
+def main():
+    args = get_arguments()
+    check_for_blast()
+    check_file_exists(args.gene_db)
+    check_algorithm(args.algorithm)
+
+    if args.report_new_consensus:
+        new_consensus_alleles = open('new_consensus_alleles.fasta', 'w')
+    if args.report_all_consensus:
+        all_consensus_alleles = open('all_consensus_alleles.fasta', 'w')
+
+    all_clusters = Set()
+    all_results = {} # key = assembly_name, value = dict of cluster and allele
+    for assembly in args.assemblies:
+        assembly_name = remove_extension_from_assembly_file(assembly)
+        all_results[assembly_name] = {}
+        blast_results = blast_assembly(assembly, args.gene_db, args.algorithm)
+        filtered_blast_results = filter_blast_results(blast_results, args.min_coverage, args.max_divergence)
+        best_hits = get_best_match_for_each_cluster(filtered_blast_results)
+        for cluster, best_hit in best_hits.iteritems():
+            all_clusters.add(cluster)
+            query_name = best_hit[0]
+            allele_name = best_hit[1]
+            identity = best_hit[2]
+            coverage = best_hit[3]
+            hit_seq = best_hit[4]
+            imperfect_match = identity < 100.0 or coverage < 100.0
+            if imperfect_match:
+                allele_name += '*'
+            all_results[assembly_name][cluster] = (query_name, allele_name, hit_seq, imperfect_match)
+
+    sorted_clusters = sorted(list(all_clusters))
+    sorted_assemblies = sorted(list(all_results.keys()))
+
+    out_file = open(args.output, 'w')
+    out_file.write('Sample\t')
+    out_file.write('\t'.join(sorted_clusters))
+    out_file.write('\n')
+    for assembly in sorted_assemblies:
+        out_file.write(assembly + '\t')
+        results = []
+        for cluster in sorted_clusters:
+            if cluster not in all_results[assembly]:
+                results.append('-')
+            else:
+                query_name = all_results[assembly][cluster][0]
+                allele_name = all_results[assembly][cluster][1]
+                hit_seq = all_results[assembly][cluster][2]
+                imperfect_match = all_results[assembly][cluster][3]
+                results.append(allele_name)
+                if args.report_all_consensus:
+                    full_query_name = query_name
+                    if imperfect_match:
+                        full_query_name += '.variant'
+                    else:
+                        full_query_name += '.consensus'
+                    add_fasta_to_file(full_query_name + ' ' + assembly, hit_seq, all_consensus_alleles)
+                if args.report_new_consensus and imperfect_match:
+                    add_fasta_to_file(query_name + '.variant ' + assembly, hit_seq, new_consensus_alleles)
+        out_file.write('\t'.join(results))
+        out_file.write('\n')
+
+
+def check_file_exists(filename):
+    if not os.path.isfile(filename):
+        print('Error: could not load ' + contig_filename, file=sys.stderr)
+        quit()
+
+
+def check_algorithm(algorithm):
+    if algorithm != 'blastn' and algorithm != 'blastn-short' and algorithm != 'megablast' and algorithm != 'dc-megablast':
+        print('Error: algorithm must be "blastn", "blastn-short", "megablast" or "dc-megablast"', file=sys.stderr)
+        quit()
+
+
+def check_for_blast():
+    makeblastdb_path = spawn.find_executable('makeblastdb')
+    blastn_path = spawn.find_executable('blastn')
+    blast_installed = (makeblastdb_path != None and blastn_path != None)
+    if not blast_installed:
+        print('Error: could not find BLAST program', file=sys.stderr)
+        quit()
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description='SRST2 results from assemblies')
+    parser.add_argument('--assemblies', nargs='+', type=str, required=True, help='Fasta file/s for assembled contigs')
+    parser.add_argument('--gene_db', type=str, required=True, help='Fasta file for gene databases')
+    parser.add_argument('--output', type=str, required=True, help='Compiled table of results')
+    parser.add_argument('--min_coverage', type=float, required=False, help='Minimum %%coverage cutoff for gene reporting (default 90)',default=90)
+    parser.add_argument('--max_divergence', type=float, required=False, help='Maximum %%divergence cutoff for gene reporting (default 10)',default=10)
+    parser.add_argument('--report_new_consensus', action="store_true", required=False, help='If a matching alleles is not found, report the consensus allele. Note, only SNP differences are considered, not indels.')
+    parser.add_argument('--report_all_consensus', action="store_true", required=False, help='Report the consensus allele for the most likely allele. Note, only SNP differences are considered, not indels.')
+    parser.add_argument('--algorithm', action="store", help="blast algorithm (blastn)", default="blastn")
+    return parser.parse_args()
+
+
+def blast_assembly(assembly, gene_db, algorithm):
+    check_file_exists(assembly)
+
+    # Make the BLAST database if it doesn't already exist.
+    if not os.path.isfile(assembly + ".nin"):
+        makeblastdb_command = ['makeblastdb', '-dbtype', 'nucl', '-in', assembly]
+        process = subprocess.Popen(makeblastdb_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = process.communicate()
+        if len(err) > 0:
+            print('\nmakeblastdb encountered an error:', file=sys.stderr)
+            print(err, file=sys.stderr)
+            quit()
+
+    blastn_command = ['blastn', '-task', algorithm, '-db', assembly, '-query', gene_db, '-outfmt', '6 qseqid pident qlen length sseq']
+    process = subprocess.Popen(blastn_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    if len(err) > 0:
+        print('\nblastn encountered an error:', file=sys.stderr)
+        print(err, file=sys.stderr)
+        quit()
+
+    blast_results = []
+
+    for line in blast_results_iterator(out):
+        line_parts = line.split('\t')
+        if len(line_parts) < 5:
+            continue
+        query_name = line_parts[0]
+        identity = float(line_parts[1])
+        query_length = float(line_parts[2])
+        hit_length = float(line_parts[3])
+        coverage = 100.0 * hit_length / query_length
+        hit_seq = line_parts[4]
+        query_name_parts = query_name.split('__')
+        if len(query_name_parts) < 4:
+            print('Error: gene database names must be in the following format:', file=sys.stderr)
+            print('[clusterUniqueIdentifier]__[clusterSymbol]__[alleleSymbol]__[alleleUniqueIdentifier]', file=sys.stderr)
+            quit()
+        cluster_name = query_name_parts[1]
+        allele_name = query_name_parts[2]
+
+        blast_results.append((query_name, cluster_name, allele_name, identity, coverage, hit_seq))
+
+    return blast_results
+
+
+def filter_blast_results(blast_results, coverage_cutoff, max_divergence):
+    identity_cutoff = 100.0 - max_divergence
+    filtered_blast_results = []
+    for result in blast_results:
+        identity = result[3]
+        coverage = result[4]
+        if identity >= identity_cutoff and coverage >= coverage_cutoff:
+            filtered_blast_results.append(result)
+    return filtered_blast_results
+
+
+def get_best_match_for_each_cluster(filtered_blast_results):
+    best_hits = {}
+    for result in filtered_blast_results:
+        query_name = result[0]
+        cluster_name = result[1]
+        allele_name = result[2]
+        identity = result[3]
+        coverage = result[4]
+        hit_seq = result[5].replace('-', '')
+        if cluster_name not in best_hits:
+            best_hits[cluster_name] = (query_name, allele_name, identity, coverage, hit_seq)
+        else:
+            previous_best_hit = best_hits[cluster_name]
+            previous_best_identity = previous_best_hit[2]
+            previous_best_coverage = previous_best_hit[3]
+            if identity > previous_best_identity or (identity == previous_best_identity and coverage > previous_best_coverage):
+                best_hits[cluster_name] = (query_name, allele_name, identity, coverage, hit_seq)
+    return best_hits
+
+
+# http://stackoverflow.com/questions/3054604/iterate-over-the-lines-of-a-string
+def blast_results_iterator(results):
+    prevnl = -1
+    while True:
+      nextnl = results.find('\n', prevnl + 1)
+      if nextnl < 0: break
+      yield results[prevnl + 1:nextnl]
+      prevnl = nextnl
+
+
+def remove_extension_from_assembly_file(assembly_filename):
+    assembly_filename = rchop(assembly_filename, '.fasta')
+    assembly_filename = rchop(assembly_filename, '.fa')
+    return assembly_filename
+
+
+# http://stackoverflow.com/questions/3663450/python-remove-substring-only-at-the-end-of-string
+def rchop(thestring, ending):
+    if thestring.endswith(ending):
+        return thestring[:-len(ending)]
+    return thestring
+
+
+def add_fasta_to_file(name, seq, file):
+    file.write('>')
+    file.write(name)
+    file.write('\n')
+    file.write(seq)
+    file.write('\n')
+
+
+
+if __name__ == '__main__':
+    main()
