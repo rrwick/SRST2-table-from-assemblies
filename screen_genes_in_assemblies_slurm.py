eba556d641eee171618ec853ed0c414a235c0c58
Yu Wan <wanyuac@126.com>
2018-11-17 10:36:51 +1100

    Update screen_genes_in_assemblies_slurm.py

--- screen_genes_in_assemblies_slurm.py
+++ screen_genes_in_assemblies_slurm.py
@@ -10,7 +10,7 @@ Previous name: srst2_table_from_assemblies_slurm.py
 
 Copyright (C) 2015-2017 Ryan Wick <rrwick@gmail.com>, Yu Wan <wanyuac@gmail.com>
 Licensed under the GNU General Public License, version 3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
-Latest edition: 9-10 Sep 2017
+First edition: 9-10 Sep 2017; the latest edition: 17 Nov 2018
 """
 
 from __future__ import print_function
@@ -29,6 +29,7 @@ def get_arguments():
     parser.add_argument("--script", type = str, required = False, default = "./screen_genes_in_assemblies.py", help = "path to screen_genes_in_assemblies.py, if not in the current directory or this script's directory")
     parser.add_argument("--walltime", type = str, required = False, default = "0-0:30:0", help = "wall time for each job bundle (default: 0-0:30:0 = 30 min)")
     parser.add_argument("--memory", type = str, required = False, default = "512", help = "memory assigned to every job (default: 512 MB)")
+    parser.add_argument("--account", type = str, required = False, default = "", help = "SLURM account name (default: null)")
     parser.add_argument("--partition", type = str, required = False, default = "main", help = "name of the queue partition (default: main)")
     parser.add_argument("--bundle_name_prefix", type = str, required = False, default = "genotyping", help = "prefix for the name of every bundle of SLURM jobs (default: genotyping)")
     parser.add_argument("--bundle_size", type = int, required = False, default = 16, help = "number of individual jobs per bundle (default: 16)")
@@ -38,10 +39,15 @@ def get_arguments():
     # script arguments
     parser.add_argument("--assemblies", nargs = "+", type = str, required = True, help = "Fasta file/s for assembled contigs")
     parser.add_argument("--gene_db", type = str, required = True, help = "Fasta file for gene databases")
-    parser.add_argument('--algorithm', type = str, required = False, default = "megablast", help="blast algorithm (megablast)")
+    parser.add_argument("--algorithm", type = str, required = False, default = "megablast", help = "blast algorithm (megablast)")
     parser.add_argument("--prefix", type = str, required = False, default = "BLAST", help = "Output prefix for the table of results")
     parser.add_argument("--suffix", type = str, required = False, default = ".fasta", help = "Characters to be chopped off from the end of every assembly name in order to get a sample name")
     parser.add_argument("--other_args", type = str, required = False, help = "A single string consisting of other arguments to be passed directly to screen_genes_in_assemblies.py")
+    
+    # environment settings
+    parser.add_argument("--blast", type = str, required = False, default = "", help = "Module name of BLAST")
+    parser.add_argument("--python", type = str, required = False, default = "", help = "Module name of Python")
+    
     return parser.parse_args()
 
 
@@ -104,15 +110,19 @@ def main():
         # submit a bundle when there are args.bundle_size jobs
         if job_count == args.bundle_size or job_left == 0:
             bundle_count += 1
-            bundle_cmd = "#!/bin/bash"
+            bundle_cmd = "#!/bin/bash\n"
+            if args.account != "":
+                bundle_cmd += "\n$SBATCH --account=" + args.account
             bundle_cmd += "\n#SBATCH --partition=" + args.partition
             bundle_cmd += "\n#SBATCH --job-name=" + args.bundle_name_prefix + "_" + str(bundle_count)
             bundle_cmd += "\n#SBATCH --ntasks=" + str(job_count)
             bundle_cmd += "\n#SBATCH --cpus-per-task=1"
             bundle_cmd += "\n#SBATCH --mem-per-cpu=" + args.memory  # memory per task (CPU) when there has to be one processor (CPU) per task
             bundle_cmd += "\n#SBATCH --time=" + args.walltime
-            bundle_cmd += "\nmodule load BLAST+/2.2.30-vlsci_intel-2015.08.25"  # change to your own module names
-            bundle_cmd += "\nmodule load Python/2.7.10-vlsci_intel-2015.08.25\n"
+            if args.blast != "":
+                bundle_cmd += "\nmodule load " + args.blast + "\n"  # change to your own module names
+            if args.python != "":
+                bundle_cmd += "\nmodule load " + args.python + "\n"
             bundle_cmd += tasks + "wait\n"  # terminate this bundle only when all tasks end
             if run:
                 os.system("echo '" + bundle_cmd + "' | sbatch")  # submit this bundle of jobs
